f547e850abd4ad4f9a89447803d93427db9e283b
surekaboobalan <surekadtarasu@gmail.com>
2024-02-19 16:47:02 +0530

    commit 1

---
+++ src/test/java/com/runner/Runner.java
@@ -0,0 +1,14 @@
+package com.runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src\\test\\java\\com\\featurefile", glue="com.stepdef",
+plugin = {"json:cucmberrreport.report1(json)"})
+public class Runner {
+
+	
+}
